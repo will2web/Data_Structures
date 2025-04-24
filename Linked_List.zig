fe6f778f3eef1e749abe21dfdfaea37da5fde4a1
William Rescigno <143554483+will2web@users.noreply.github.com>
2025-04-24 10:13:31 -0700

    Create Linked_List.zig
    Created an outline; started a scaffolding a few basic functions

---
+++ Linked_List.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+const debug_print = std.debug.print;
+
+const Node = struct {
+    value: u8,
+    next_node_pointer: u8,
+};
+
+pub fn main() !void {
+    //ask user 1st value
+    //call createLinkedList using that value
+
+}
+
+// Create Linked List (basically, Create 1st node)
+fn createLinkedList(first_value: u8) !void {
+    //create linked list with 1st node
+    createNode(first_value, null);
+    //therefore call createNode
+}
+
+fn createNode(node_value: u8, node_pointer: usize) !Node {
+    const new_Node: Node = .{ node_value, node_pointer };
+    return new_Node;
+}
+
+fn insertNode() !void {}
+// Insertion: Adding a new node to the list, either at the beginning, end, or a specific position.
+
+fn deleteNode() !void {}
+// Deletion: Removing a node from the list, again at the beginning, end, or a specific position.
+
+fn traverseLinkedList() !void {}
+// Traversal: Accessing each node in the list, usually sequentially.
+
+fn searchLinkedList() !void {}
+// Search: Finding a specific node based on its data value or position.
+
+fn sortLinkedList() !void {}
+// Sort: Ordering the nodes in the list based on their data values.
