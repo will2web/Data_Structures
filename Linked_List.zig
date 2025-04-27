28dbe99b1ac1d8a7c890bb1cebd5f32404218469
William Rescigno <143554483+will2web@users.noreply.github.com>
2025-04-27 10:04:17 -0700

    Update Linked_List.zig
    I have each node referencing the next correctly, and I'm able to print out the values of all of the nodes; All of this is currently taking place in the main function, which I will refactor as I progress

--- Linked_List.zig
+++ Linked_List.zig
@@ -3,21 +3,36 @@ const debug_print = std.debug.print;
 
 const Node = struct {
     value: u8,
+    //Should next node pointer point to a node, or another pointer?
     next_node_pointer: ?*Node,
 };
 
 pub fn main() !void {
-    // const Node = struct {
-    //     value: u8,
-    //     next_node_pointer: ?*Node,
-    // };
+    var Node1: Node = .{ .value = 87, .next_node_pointer = null };
+    var Node2: Node = .{ .value = 17, .next_node_pointer = &Node1 };
+    var Node3: Node = .{ .value = 3, .next_node_pointer = &Node2 };
+    // debug_print("Node3: {any}\n", .{Node3});
+    var Node4: Node = .{ .value = 23, .next_node_pointer = &Node3 };
+    // debug_print("Node4: {any}\n", .{Node4});
+    var Node5: Node = .{ .value = 9, .next_node_pointer = &Node4 };
+    // debug_print("Node5: {any}\n", .{Node5});
 
+    var while_loop_pointer: ?*Node = &Node5;
+    debug_print("while_loop_pointer: {any}\n", .{while_loop_pointer});
+    debug_print("while_loop_pointer: {any}\n", .{while_loop_pointer.?.value});
+    // //while (@field(while_loop_pointer, "next_node_pointer")) {
+    while (while_loop_pointer != null) {
+        debug_print("while_loop_pointer.?.value: {any}\n", .{while_loop_pointer.?.value});
+        while_loop_pointer = while_loop_pointer.?.next_node_pointer;
+    }
+
+    //debug_print("@TypeOf(Node1) {any}", .{@TypeOf(Node)});
     //(in later versions) ask user 1st value
     //call createLinkedList using that value
-    const Node1 = createNode(87, null);
-    debug_print("Node1: {any}\n\n", .{Node1});
-    //debug_print("@typeInfo(Node1) {any}", .{@typeInfo(Node1)});
-    debug_print("@typeInfo(Node1) {any}", .{@typeInfo(Node)});
+    //const Node1 = createNode(87, null); //might be used when I break code into functions
+
+    //debug_print("@typeInfo(Node1) {any}", .{@type_Name(Node1)});
+
     //debug_print("@Type(Node1) {any}", .{@Type(Node1)});
     //    try debug_print("value1: {d}", .{Node1.value});
     //try debug_print("next_node_pointer: {any}", .{Node1.next_node_pointer});
